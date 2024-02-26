501a3e31568621fab4f84f51b301234529263e1b
BertDrei <eubertandrei.esguerra@cvsu.edu.ph>
2024-02-26 19:34:45 +0800

    fixing solution

--- TwoSum/Solution.java
+++ TwoSum/Solution.java
@@ -3,31 +3,17 @@ import java.util.Scanner;
 import java.util.Arrays;
 public class Solution {
 
-    public static void main(String[] args) {
+   public int[] TwoSum (int[] nums, int target) {
 
-        Scanner scan = new Scanner(System.in);
+       for(int i = 0; i < nums.length; i++){
+           for(int j = i + 1; j < nums.length; j++){
+               if( nums[i] + nums[j] == target){
+                   return new int[]{i, j};
+               }
+           }
 
-        int[] nums = {2,7,11,15};
-        int target  = 18;
+       }
+       return nums;
 
-        //int[] answer = twoSum(nums, target);
-
-       // System.out.println(Arrays.toString(answer));
-    }
-
-
-
-
-    public int[] twoSum(int[] nums, int target) {
-
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
-            }
-
-        }
-        return nums;
     }
 }
